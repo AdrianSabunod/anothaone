fb4da3385e598c13296f31ada1b90d133d7355ca
AdrianSabunod <20180014639@my.xu.edu.ph>
2023-05-05 21:52:57 +0800

    Initial commit

---
+++ WaterDelivery/app/src/main/java/com/example/waterdelivery/Animation_tubigActivity.kt
@@ -0,0 +1,11 @@
+package com.example.waterdelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Animation_tubigActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animation_tubig)
+    }
+}
