90f468498b95fc770d73f66049349fb75877d9c0
Adrian Jose J. Sabunod <20180014639@my.xu.edu.ph>
2023-05-06 03:05:44 +0800

    you know

--- WaterDelivery/app/src/main/java/com/example/waterdelivery/Animation_tubigActivity.kt
+++ WaterDelivery/app/src/main/java/com/example/waterdelivery/Animation_tubigActivity.kt
@@ -1,11 +1,38 @@
 package com.example.waterdelivery
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.waterdelivery.R.id
+import com.example.waterdelivery.R.layout
+
 
 class Animation_tubigActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_animation_tubig)
+        setContentView(layout.activity_animation_tubig)
+        supportActionBar?.hide()
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+
+        val textView = findViewById<TextView>(id.TubigSplashText)
+        textView.animate().translationX(1000F).setDuration(1000).setStartDelay(2500)
+
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(4777)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    val intent = Intent(this@Animation_tubigActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+        thread.start()
     }
 }
